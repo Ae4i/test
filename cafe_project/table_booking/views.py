e6c951a3cf510110eb6172a8b222011e6d16ef31
Ae4i <artemisaenko2009@gmail.com>
2026-01-30 16:29:08 +0100

    add views

--- cafe_project/table_booking/views.py
+++ cafe_project/table_booking/views.py
@@ -11,3 +11,24 @@ def tables_list(request):
     tables = Table.objects.filter(is_active=True)
     context = {"tables": tables}
     return render(request,"tables_list.html",context)
+
+def table_detail(request,table_id):
+    table = get_object_or_404(Table, id=table_id)
+    return render(request,"table_detail.html",{"table":table})
+
+def client_detail(request,client_id):
+    client = get_object_or_404(Client, id=client_id)
+    bookings = Booking.objects.filter(client=client)
+    return render(request,"client_detail.html",{"client":client,"bookings":bookings})
+
+def bookings_by_date(request):
+    date = request.GET.get('date')
+    bookings = None
+    if date:
+        bookings = Booking.objects.filter(date=date)
+    return render(request,"bookings_by_date.html",{"bookings":bookings, "date":date})
+
+def table_bookings(request, table_id):
+    table = get_object_or_404(Table, id=table_id)
+    bookings = Booking.objects.filter(table=table)
+    return render(request,"table_bookings.html",{"table":table,"bookings":bookings})
